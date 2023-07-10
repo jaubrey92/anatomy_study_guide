f7ef271fcef00e0a3eb7fa2f7bad0818bbcab218
James Aubrey <jaaubrey92@gmail.com>
2023-07-10 15:19:04 -0500

    muscle index axios call

--- client/src/components/Flashcard.jsx
+++ client/src/components/Flashcard.jsx
@@ -1,5 +1,19 @@
-const Flashcard = () => {
-  return <div></div>
+const Flashcard = (props) => {
+  return (
+    <div key={props.id}>
+      <div>
+        <img src={props.image} alt="Muscle Image" />
+      </div>
+      <div>
+        <h3>{props.title}</h3>
+        <p>{props.origin}</p>
+        <p>{props.insertion}</p>
+        <p>{props.innervation}</p>
+        <p>{props.action}</p>
+        {/* <p>{props.comments}</p> */}
+      </div>
+    </div>
+  )
 }
 
 export default Flashcard
--- client/src/pages/MuscleIndex.jsx
+++ client/src/pages/MuscleIndex.jsx
@@ -1,7 +1,44 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import Flashcard from '../components/Flashcard'
+import { Link } from 'react-router-dom'
+import { GetCards } from '../services/CardServices'
+
 const MuscleIndex = () => {
+  let { cardId } = useParams()
+
+  const [cards, setCards] = useState([])
+
+  const getAllFlashcards = async () => {
+    let response = await GetCards()
+    console.log(response)
+    setCards(response)
+  }
+
+  useEffect(() => {
+    getAllFlashcards()
+  }, [])
+
   return (
     <div>
       <h1>All Muscles</h1>
+      <div>
+        {cards.map((card) => (
+          <Link to={`/cards/details/${cardId}`}>
+            <Flashcard
+              id={card._id}
+              title={card.title}
+              origin={card.origin}
+              insertion={card.insertion}
+              innervation={card.innervation}
+              action={card.action}
+              image={card.image}
+              // comments={card.comments.content}
+              // onClick={onClick}
+            />
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
