575f2a8d7871a7514d41cff01ba6509777227983
James Aubrey <jaaubrey92@gmail.com>
2023-07-10 12:53:56 -0500

    app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+import About from './pages/About'
+import MuscleIndex from './pages/MuscleIndex'
+import RegionIndex from './pages/RegionIndex'
 
 const App = () => {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/cards/details/:cardId" element={<MuscleIndex />} />
+          <Route path="/view/cards/:regionId" element={<RegionIndex />} />
+        </Routes>
+      </main>
+    </div>
+  )
 }
 
 export default App
